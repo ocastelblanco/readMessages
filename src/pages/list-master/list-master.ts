c46d05a7f17bd53bc9f476d681026fcfc24204f3
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2018-04-27 19:23:18 -0500

    Read a single message

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,54 +1,53 @@
 import { Component } from '@angular/core';
-import { IonicPage, ModalController, NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { Item } from '../../models/item';
-import { Items } from '../../providers/providers';
 
 @IonicPage()
 @Component({
-  selector: 'page-list-master',
-  templateUrl: 'list-master.html'
+    selector: 'page-list-master',
+    templateUrl: 'list-master.html'
 })
 export class ListMasterPage {
-  currentItems: Item[];
-
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
-    this.currentItems = this.items.query();
-  }
-
-  /**
-   * The view loaded, let's query our items for the list
-   */
-  ionViewDidLoad() {
-  }
-
-  /**
-   * Prompt the user to add a new item. This shows our ItemCreatePage in a
-   * modal and then adds the new item to our data source if the user created one.
-   */
-  addItem() {
-    let addModal = this.modalCtrl.create('ItemCreatePage');
-    addModal.onDidDismiss(item => {
-      if (item) {
-        this.items.add(item);
-      }
-    })
-    addModal.present();
-  }
-
-  /**
-   * Delete an item from the list of items.
-   */
-  deleteItem(item) {
-    this.items.delete(item);
-  }
-
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
-  }
+    mensajes: Observable<any[]>;
+    todosMensajes: Array;
+    constructor(public navCtrl: NavController,
+                 public db: AngularFireDatabase,
+                 public afAuth: AngularFireAuth
+                ) {
+        const hoy = Date.now();
+        this.mensajes = db.list('mensajes', ref => ref.orderByChild('date')).valueChanges().subscribe(mensaje => {
+            this.todosMensajes = mensaje.reverse();
+            this.todosMensajes.forEach((elemento, index) => {
+                const fecha = new Date(elemento.date);
+                const tiempo = hoy - fecha.getTime();
+                const diferencia;
+                if (tiempo < (120 * 60 * 1000)) {
+                    diferencia = String(Math.ceil(tiempo / (60 * 1000)))+' min.';
+                } else if (tiempo < (24 * 3600 * 1000)) {
+                    diferencia = String(Math.floor(tiempo / (3600 * 1000)))+' horas';
+                } else if (tiempo < (7 * 24 * 3600 * 1000)) {
+                    diferencia = String(Math.floor(tiempo / (24 * 3600 * 1000)))+' días';
+                } else if (tiempo < (4 * 7 * 24 * 3600 * 1000)) {
+                    diferencia = String(Math.floor(tiempo / (7 * 24 * 3600 * 1000)))+' sem.';
+                } else if (tiempo < (12 * 4 * 7 * 24 * 3600 * 1000)) {
+                    diferencia = String(Math.floor(tiempo / (4 * 7 * 24 * 3600 * 1000)))+' meses';
+                } else {
+                    diferencia =String(Math.floor(tiempo / (12 * 4 * 7 * 24 * 3600 * 1000)))+' años';
+                }
+                this.todosMensajes[index].diferencia = diferencia;
+            });
+        });
+    }
+    abreMensaje(mensaje: Item) {
+        this.navCtrl.push('ItemDetailPage', {
+            item: mensaje
+        });
+    }
+    logOut() {
+        this.afAuth.auth.signOut();
+        //this.navCtrl.push('LoginPage');
+    }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,38 +2,56 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { User } from '../../providers/providers';
 import { MainPage } from '../pages';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
 })
 export class LoginPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
-  };
+    // The account fields for the login form.
+    // If you're using the username field with or without email, make
+    // sure to add it to the type
+    account: {
+        email: string,
+        password: string
+    } = {
+        email: 'ocastelblanco@gmail.com',
+        password: ''
+    };
 
-  // Our translated text strings
-  private loginErrorString: string;
+    // Our translated text strings
+    private loginErrorString: string;
 
-  constructor(public navCtrl: NavController,
-    public user: User,
-    public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+    constructor(public navCtrl: NavController,
+                 public user: User,
+                 public toastCtrl: ToastController,
+                 public translateService: TranslateService,
+                 private afAuth: AngularFireAuth) {
 
-    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
-      this.loginErrorString = value;
-    })
-  }
+        this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+            this.loginErrorString = value;
+        })
+    }
 
-  // Attempt to login in through our User service
-  doLogin() {
+    // Attempt to login in through Firebase
+    async doLogin(account) {
+//        async login() {
+            try {
+                const result = await this.afAuth.auth.signInWithEmailAndPassword(account.email, account.password);
+                if (result) {
+                    this.navCtrl.setRoot('ListMasterPage');
+                }  
+            }
+            catch (e) {
+                console.error(e);
+            }
+        }
+        /*
     this.user.login(this.account).subscribe((resp) => {
       this.navCtrl.push(MainPage);
     }, (err) => {
@@ -46,5 +64,5 @@ export class LoginPage {
       });
       toast.present();
     });
-  }
+*/
 }
